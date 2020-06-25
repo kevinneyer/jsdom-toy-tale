bf0337f73cd5ee68cdff1239180bef6f6c0e329a
kevinneyer <kevinneyer@gmail.com>
2020-06-25 15:23:51 -0400

    Kevin's First Commit

--- src/index.js
+++ src/index.js
@@ -12,4 +12,30 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+
+  fetch("http://localhost:3000/toys")
+  .then(response => response.json())
+  .then(data => {
+    const toyCollection = document.querySelector("#toy-collection")
+    const toyDiv = document.createElement('div')
+    toyDiv.class = "card"
+
+    data.forEach(toy => { 
+     toyDiv.innerHTML += `
+     <h2>${toy.name}</h2>
+     <img src=${toy.image} class="toy-avatar" />
+     <p>${toy.likes} Likes </p>
+     <button class="like-btn">Like <3</button>
+     `
+     })
+     
+     
+
+     toyCollection.append(toyDiv)
+     console.log(toyCollection)
+
+  })
+
+
+
 });
